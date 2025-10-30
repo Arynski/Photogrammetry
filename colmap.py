43c7f1f889e7a1313695fe28823dbc9cccf6b945
laggenerator <tomaszgasior30+github@gmail.com>
2025-10-30 15:13:55 +0100

    mozna podac nazwe w argumencie i zrespi sie w katalogu chmury

--- colmap.py
+++ colmap.py
@@ -4,6 +4,8 @@ import open3d as o3d
 import numpy as np
 import matplotlib as plt
 import trimesh
+import sys
+import shutil
 
 n_watkow = 8 # Ile wątków
 uzywacGPU = False # Czy robic z GPU
@@ -13,63 +15,75 @@ zdjecia_dir = Path("./zdjecia")
 output_dir = Path("./output")
 reco_dir = Path("./output/reconstructions")
 undistort_dir = Path("./undistort")
+chmury_dir = Path("./chmury")
 
 # Tworzenie output jeżeli go nima
 output_dir.mkdir(exist_ok=True)
 reco_dir.mkdir(exist_ok=True)
-undistort_dir = Path(exists_ok=True)
+undistort_dir.mkdir(exist_ok=True)
+chmury_dir.mkdir(exist_ok=True)
 
-
-
-# Ekstrakcja ficzerów
-pycolmap.extract_features(
-  database_path = output_dir / "bazunia.db",
-  image_path = zdjecia_dir,
-  camera_mode=pycolmap.CameraMode.AUTO,
-  sift_options=pycolmap.SiftExtractionOptions(
-    num_threads=n_watkow,
-    use_gpu=uzywacGPU,
-    max_num_features = 3000
-  )
-)
-
-# Matching ficzerów
-pycolmap.match_sequential(
-  database_path = output_dir / "bazunia.db",
-  sift_options=pycolmap.SiftMatchingOptions(
-    num_threads=n_watkow,
-    use_gpu=uzywacGPU
-  )
-)
-
-# Rekonstrukcja
-def wlacz_rekonstrukcje(zdjecia_dir, output_dir):
-  rekonstrukcja = pycolmap.incremental_mapping(
-    database_path=output_dir/ "bazunia.db",
-    image_path=zdjecia_dir,
-    output_path=output_dir,
-    options=pycolmap.IncrementalPipelineOptions(
+def czy_istnieje_rekonstrukcja():
+    ply_path = output_dir / "chmurka.PLY"
+    
+    if reco_dir.exists() and ply_path.exists():
+        if any(reco_dir.iterdir()):
+            print("Znaleziono istniejącą rekonstrukcję")
+            return True
+    return False
+def colmap():
+  # Ekstrakcja ficzerów
+  pycolmap.extract_features(
+    database_path = output_dir / "bazunia.db",
+    image_path = zdjecia_dir,
+    camera_mode=pycolmap.CameraMode.AUTO,
+    sift_options=pycolmap.SiftExtractionOptions(
       num_threads=n_watkow,
-      ba_use_gpu=uzywacGPU
+      use_gpu=uzywacGPU,
+      max_num_features = 3000
     )
   )
-  return rekonstrukcja
 
-# Odpalajjj to
-#zwraca slownik, gdzie klucz to int takie id, jesli bedzie
-#pare rekonstrukcji to beda numerowane 0, 1, 2, ...
-rek_dict = wlacz_rekonstrukcje(zdjecia_dir, output_dir)
+  # Matching ficzerów
+  pycolmap.match_sequential(
+    database_path = output_dir / "bazunia.db",
+    sift_options=pycolmap.SiftMatchingOptions(
+      num_threads=n_watkow,
+      use_gpu=uzywacGPU
+    )
+  )
 
-#dla ulatwienia na poczatku wezmy tylko pierwsze
-if rek_dict[0] is not None:
-    rek = rek_dict[0]
-    print(f"Zrekonstruowano coś!")
-    print(f"Zarejestrowano {len(rek.images)} zdjęć")
-    print(f"Utworzono {len(rek.points3D)} punktów 3D")
-    rek.export_PLY(output_dir / "chmurka.PLY")
-    rek.write(output_dir / "reconstructions")
+  # Rekonstrukcja
+  def wlacz_rekonstrukcje(zdjecia_dir, output_dir):
+    rekonstrukcja = pycolmap.incremental_mapping(
+      database_path=output_dir/ "bazunia.db",
+      image_path=zdjecia_dir,
+      output_path=output_dir,
+      options=pycolmap.IncrementalPipelineOptions(
+        num_threads=n_watkow,
+        ba_use_gpu=uzywacGPU
+      )
+    )
+    return rekonstrukcja
 
-rek_dict[0].write(output_dir / "reconstructions")
+  # Odpalajjj to
+  #zwraca slownik, gdzie klucz to int takie id, jesli bedzie
+  #pare rekonstrukcji to beda numerowane 0, 1, 2, ...
+  rek_dict = wlacz_rekonstrukcje(zdjecia_dir, output_dir)
+
+  #dla ulatwienia na poczatku wezmy tylko pierwsze
+  if rek_dict[0] is not None:
+      rek = rek_dict[0]
+      print(f"Zrekonstruowano coś!")
+      print(f"Zarejestrowano {len(rek.images)} zdjęć")
+      print(f"Utworzono {len(rek.points3D)} punktów 3D")
+      rek.export_PLY(output_dir / "chmurka.PLY")
+      rek.write(output_dir / "reconstructions")
+
+  rek_dict[0].write(output_dir / "reconstructions")
+
+if not czy_istnieje_rekonstrukcja():
+   colmap()
 zaladowana_rekonstrukcja = pycolmap.Reconstruction(output_dir / "reconstructions")
 
 #odzniekształcanie (undistort lol) zdjęć i zapisywanie ich i innych rzeczy
@@ -102,22 +116,9 @@ subprocess.run(
   )
 #w ./undistort/pmvs/models wypluje model o nazwie <nazwa pliku z opcjami>.ply xD
 
-
-pcd = o3d.io.read_point_cloud(os.path.join(prefix, "models/", option_file, ".ply"))
-pcd.estimate_normals()
-
-distances = pcd.compute_nearest_neighbor_distance()
-avg_dist = np.mean(distances)
-radius = 1.5 * avg_dist   
-
-mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(
-           pcd,
-           o3d.utility.DoubleVector([radius, radius * 2]))
-
-# create the triangular mesh with the vertices and faces from open3d
-tri_mesh = trimesh.Trimesh(np.asarray(mesh.vertices), np.asarray(mesh.triangles),
-                          vertex_normals=np.asarray(mesh.vertex_normals))
-
-trimesh.convex.is_convex(tri_mesh)
-
-o3d.visualization.draw_plotly(tri_mesh)
+if(len(sys.argv) > 1):
+  stara_nazwa = option_file + ".ply"
+  nowa_nazwa = sys.argv[1] + ".ply"
+  stary_plik = os.path.join(base_dir, "undistort/pmvs/models/", stara_nazwa)
+  nowy_plik = os.path.join(base_dir, "chmury/", nowa_nazwa)
+  shutil.move(str(stary_plik), str(nowy_plik))
