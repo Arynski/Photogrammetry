b6af4d9d4054d92ac3bbb41ffa84915abb475fff
laggenerator <tomaszgasior30+github@gmail.com>
2025-11-15 12:06:14 +0100

    wybieranie rozdzielczosci w gui

--- main.py
+++ main.py
@@ -4,15 +4,14 @@ import os
 import sys
 from PyQt6.QtWidgets import (QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, 
                             QWidget, QPushButton, QLabel, QLineEdit, QFileDialog, 
-                            QMessageBox, QProgressBar, QTextEdit)
+                            QMessageBox, QProgressBar, QTextEdit, QLabel, QComboBox, QVBoxLayout, QWidget)
 from PyQt6.QtCore import Qt, QThread, pyqtSignal
 import math
 import glob
 import subprocess
 
 def wyczyscZdjecia(log_callback=None):
-
-    files = glob.glob('.work/zdjecia/*')
+    files = glob.glob('work/zdjecia/*')
     pomyslnieusuniete = 0
     nieusuniete = 0
     for f in files:
@@ -22,14 +21,18 @@ def wyczyscZdjecia(log_callback=None):
                 pomyslnieusuniete += 1
         except Exception as e:
             print(f'Błąd przy usuwaniu zdjęcia -> {e}')
-            log_callback(f'Błąd przy usuwaniu zdjęcia -> {e}')
+            if log_callback:
+                log_callback(f'Błąd przy usuwaniu zdjęcia -> {e}')
             nieusuniete += 1
     print(f'Usunięto {pomyslnieusuniete}, nie udało się usunąć {nieusuniete}')
-    log_callback(f'Usunięto {pomyslnieusuniete}, nie udało się usunąć {nieusuniete}')
+    if log_callback:
+        log_callback(f'Usunięto {pomyslnieusuniete}, nie udało się usunąć {nieusuniete}')
 
-def ekstracjaKlatek(sciezka, docelowa_liczba_klatek, log_callback=None):
+def ekstracjaKlatek(sciezka, docelowa_liczba_klatek, docelowa_rozdzielczosc=None, log_callback=None):
     print(f'Otwieranie filmu: {sciezka}')
-    log_callback(f'Otwieranie filmu: {sciezka}')
+    if log_callback:
+        log_callback(f'Otwieranie filmu: {sciezka}')
+    
     film = cv.VideoCapture(sciezka)
     
     # Pobierz całkowitą liczbę klatek w filmie
@@ -43,32 +46,43 @@ def ekstracjaKlatek(sciezka, docelowa_liczba_klatek, log_callback=None):
         interwal = max(1, calkowita_liczba_klatek // docelowa_liczba_klatek)
     
     print(f'Film ma {calkowita_liczba_klatek} klatek, ekstrakcja co {interwal} klatki')
+    if log_callback:
+        log_callback(f'Film ma {calkowita_liczba_klatek} klatek, ekstrakcja co {interwal} klatki')
     print(f'Docelowa liczba klatek: {docelowa_liczba_klatek}')
-    
+    if log_callback:
+        log_callback(f'Docelowa liczba klatek: {docelowa_liczba_klatek}')
+
     i = 0
     klatki = 0
     zapisane_klatki = 0
-    os.makedirs('.work/zdjecia', exist_ok=True)
+    os.makedirs('work/zdjecia', exist_ok=True)
     wyczyscZdjecia(log_callback)
     
     while(film.isOpened()):
         flag, klatka = film.read()
-
         if flag == False:
             break
         h, w, _ = klatka.shape
+        
         if klatki % interwal == 0 and zapisane_klatki < docelowa_liczba_klatek:  
-            nowew, noweh = 0, 0
-            if(h > w):
-                noweh = 1280
-                nowew = 720
+            # Docelowa rozdzielczosc, chyba ze nie wybrana to HD
+            if docelowa_rozdzielczosc:
+                nowew, noweh = docelowa_rozdzielczosc
+                if(h > w):
+                    nowew, noweh = noweh, nowew
             else:
-                noweh = 720
-                nowew = 1280
-            resized_img = cv.resize(klatka, (nowew, noweh));
+                if(h > w):
+                    noweh = 1280
+                    nowew = 720
+                else:
+                    noweh = 720
+                    nowew = 1280
+            
+            resized_img = cv.resize(klatka, (nowew, noweh))
             cv.imwrite(f'./work/zdjecia/img_{i:04d}.jpg', resized_img)
             print(f'Zapisano ./work/zdjecia/img_{i:04d}.jpg')
-            log_callback(f'Zapisano ./work/zdjecia/img_{i:04d}.jpg')
+            if log_callback:
+                log_callback(f'Zapisano ./work/zdjecia/img_{i:04d}.jpg')
             i += 1
             zapisane_klatki += 1
             
@@ -115,24 +129,24 @@ class ColmapThread(QThread):
         except Exception as e:
             self.err.emit(str(e))
 
-
-
 class ExtractionThread(QThread):
     progres = pyqtSignal(int)
     koniec = pyqtSignal(int)
     err = pyqtSignal(str)
     log_signal = pyqtSignal(str)
 
-    def __init__(self, sciezka, docelowa_liczba_klatek):
+    def __init__(self, sciezka, docelowa_liczba_klatek, docelowa_rozdzielczosc=None):
         super().__init__()
         self.sciezka = sciezka
         self.docelowa_liczba_klatek = docelowa_liczba_klatek
+        self.docelowa_rozdzielczosc = docelowa_rozdzielczosc
 
     def run(self):
         try:
             wyekstraktowane_klatki = ekstracjaKlatek(
                 self.sciezka, 
                 self.docelowa_liczba_klatek, 
+                self.docelowa_rozdzielczosc,
                 log_callback=self.log_signal.emit
             )
             self.koniec.emit(wyekstraktowane_klatki)
@@ -142,6 +156,7 @@ class ExtractionThread(QThread):
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.og_rozdzielczosc = None
         self.initUI()
 
     def initUI(self):
@@ -174,6 +189,7 @@ class MainWindow(QMainWindow):
         self.klatki_label = QLabel('Docelowa liczba klatek:')
         self.klatki_input = QLineEdit()
         self.klatki_input.setText('100')  # Domyślna wartość
+        self.klatki_input.setMaximumWidth(60)
         self.klatki_input.setPlaceholderText('Wprowadź docelową liczbę klatek')
 
         self.calkowite_klatki_label = QLabel('Całkowita liczba klatek: -')
@@ -181,8 +197,30 @@ class MainWindow(QMainWindow):
 
         klatki_layout.addWidget(self.klatki_label)
         klatki_layout.addWidget(self.klatki_input)
+        klatki_layout.addStretch()
         klatki_layout.addWidget(self.calkowite_klatki_label)
         main_layout.addLayout(klatki_layout)
+
+        # Rozdzielczość
+        rozdzielczosc_layout = QHBoxLayout()
+        self.rozdzielczosc_label = QLabel('Rozdzielczość: -')
+        self.rozdzielczosc_label.setStyleSheet("color: gray; font-style: italic;")
+
+        self.docelowa_rozdzielczosc_label = QLabel('Docelowa rozdzielczość:')
+        self.rozdzielczosc_combo = QComboBox()
+        
+        self.typowe_rozdzielczosci = [
+            (3840, 2160, "4K (3840x2160)"),
+            (2560, 1440, "1440p (2560x1440)"),
+            (1920, 1080, "1080p (1920x1080)"),
+            (1280, 720, "720p (1280x720)"),
+            (854, 480, "480p (854x480)")
+        ]
+
+        rozdzielczosc_layout.addWidget(self.rozdzielczosc_label)
+        rozdzielczosc_layout.addWidget(self.docelowa_rozdzielczosc_label)
+        rozdzielczosc_layout.addWidget(self.rozdzielczosc_combo)
+        main_layout.addLayout(rozdzielczosc_layout)
 
         # Progress bar
         self.progress_bar = QProgressBar()
@@ -194,16 +232,10 @@ class MainWindow(QMainWindow):
         self.extract_btn.clicked.connect(self.start_extract)
         main_layout.addWidget(self.extract_btn)
 
-        # Status
-        self.status_label = QLabel('Gotowy do pracy')
-        self.status_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        main_layout.addWidget(self.status_label)
-
         colmap_layout = QHBoxLayout()
         self.nazwa_modelu_label = QLabel('Nazwa modelu 3D:')
         self.nazwa_modelu_input = QLineEdit()
         self.nazwa_modelu_input.setPlaceholderText('Wprowadź nazwę pliku .ply')
-
 
         colmap_layout.addWidget(self.nazwa_modelu_label)
         colmap_layout.addWidget(self.nazwa_modelu_input)
@@ -229,6 +261,32 @@ class MainWindow(QMainWindow):
         self.log_window.setMinimumHeight(150)
         main_layout.addWidget(self.log_window)
 
+    def update_rozdzielczosc_combo(self):
+        if not self.og_rozdzielczosc:
+            return
+            
+        og_w, og_h = self.og_rozdzielczosc
+        self.rozdzielczosc_combo.clear()
+        
+        # OG rozdzielczość jako pierwsza pozycja
+        self.rozdzielczosc_combo.addItem(
+            f"Oryginalna ({og_w}x{og_h})", 
+            (og_w, og_h)
+        )
+        # Dodawania rozdzielczosci <= od oryginalnej
+        added_count = 0
+        for width, height, name in self.typowe_rozdzielczosci:
+            if width < og_w and height < og_h:
+                self.rozdzielczosc_combo.addItem(name, (width, height))
+                added_count += 1
+                
+        # Jak zadnej nie dodalo to przyps bo co to za mala rozdzielczosc lol
+
+    def wybrana_rozdzielczosc(self):
+        if self.rozdzielczosc_combo.count() > 0:
+            return self.rozdzielczosc_combo.currentData()
+        return None
+
     def log(self, message):
         self.log_window.append(message)
         self.log_window.verticalScrollBar().setValue(self.log_window.verticalScrollBar().maximum())
@@ -249,26 +307,42 @@ class MainWindow(QMainWindow):
         if film.isOpened():
             calkowita_liczba_klatek = int(film.get(cv.CAP_PROP_FRAME_COUNT))
             fps = film.get(cv.CAP_PROP_FPS)
-            film.release()
-            
-            if fps > 0:
-                duration = calkowita_liczba_klatek / fps
-                minutes = int(duration // 60)
-                seconds = int(duration % 60)
-                czas_info = f" ({minutes:02d}:{seconds:02d})"
-            else:
-                czas_info = ""
+            ret, frame = film.read()
+            if ret:
+                og_h, og_w, _ = frame.shape
+                self.og_rozdzielczosc = (og_w, og_h)
                 
-            self.calkowite_klatki_label.setText(f"Całkowita liczba klatek: {calkowita_liczba_klatek}{czas_info}")
-            self.calkowite_klatki_label.setStyleSheet("color: white; font-style: normal;")
+                self.update_rozdzielczosc_combo()
+                
+                if fps > 0:
+                    duration = calkowita_liczba_klatek / fps
+                    minutes = int(duration // 60)
+                    seconds = int(duration % 60)
+                    czas_info = f" ({minutes:02d}:{seconds:02d})"
+                else:
+                    czas_info = ""
+                    
+                self.calkowite_klatki_label.setText(f"Całkowita liczba klatek: {calkowita_liczba_klatek}{czas_info}")
+                self.calkowite_klatki_label.setStyleSheet("color: white; font-style: normal;")
+
+                self.rozdzielczosc_label.setText(f"Rozdzielczość: {og_w}x{og_h}")
+                self.rozdzielczosc_label.setStyleSheet("color: white; font-style: normal;")
+            else:
+                self.show_error("Błąd odczytu klatki")
         else:
-            self.calkowite_klatki_label.setText("Całkowita liczba klatek: Błąd odczytu")
-            self.calkowite_klatki_label.setStyleSheet("color: red; font-style: italic;")
+            self.show_error("Błąd otwarcia filmu")
+        
+        film.release()
+
+    def show_error(self, message):
+        self.calkowite_klatki_label.setText("Całkowita liczba klatek: Błąd odczytu")
+        self.calkowite_klatki_label.setStyleSheet("color: red; font-style: italic;")
+        self.rozdzielczosc_label.setText("Rozdzielczość: Błąd odczytu")
+        self.rozdzielczosc_label.setStyleSheet("color: red; font-style: italic;")
 
     def start_extract(self):
         sciezka_filmu = self.film_sciezka.text()
         klatki_text = self.klatki_input.text()
-
 
         # Walidacja
         if not sciezka_filmu:
@@ -287,14 +361,19 @@ class MainWindow(QMainWindow):
             QMessageBox.warning(self, 'Błąd', 'Proszę wprowadzić poprawną liczbę klatek!')
             return
 
+        docelowa_rozdzielczosc = self.wybrana_rozdzielczosc()
+
         # Wyłącz UI podczas ekstrakcji
         self.extract_btn.setEnabled(False)
         self.przegladaj_przycisk.setEnabled(False)
         self.progress_bar.setVisible(True)
-        self.status_label.setText('Ekstrakcja w toku...')
 
         # Rozpocznij ekstrakcję w osobnym wątku
-        self.extraction_thread = ExtractionThread(sciezka_filmu, docelowa_liczba_klatek)
+        self.extraction_thread = ExtractionThread(
+            sciezka_filmu, 
+            docelowa_liczba_klatek, 
+            docelowa_rozdzielczosc
+        )
         self.extraction_thread.koniec.connect(self.ekstrakcja_zakonczona)
         self.extraction_thread.err.connect(self.ekstrakcja_bledu)
         self.extraction_thread.log_signal.connect(self.log)
@@ -306,8 +385,7 @@ class MainWindow(QMainWindow):
         self.przegladaj_przycisk.setEnabled(True)
         self.progress_bar.setVisible(False)
         
-        self.status_label.setText(f'Ekstrakcja zakończona! Zapisano {liczba_klatek} klatek.')
-        QMessageBox.information(self, 'Sukces', f'Pomyślnie zapisano {liczba_klatek} klatek w folderze ./zdjecia/')
+        QMessageBox.information(self, 'Sukces', f'Pomyślnie zapisano {liczba_klatek} klatek w folderze ./work/zdjecia/')
 
     def ekstrakcja_bledu(self, komunikat_bledu):
         # Włącz UI ponownie
@@ -315,9 +393,7 @@ class MainWindow(QMainWindow):
         self.przegladaj_przycisk.setEnabled(True)
         self.progress_bar.setVisible(False)
         
-        self.status_label.setText('Błąd podczas ekstrakcji')
         QMessageBox.critical(self, 'Błąd', f'Wystąpił błąd: {komunikat_bledu}')
-
 
     def uruchom_colmap(self):
         nazwa_modelu = self.nazwa_modelu_input.text().strip()
@@ -339,7 +415,6 @@ class MainWindow(QMainWindow):
         self.uruchom_colmap_btn.setEnabled(False)
         self.extract_btn.setEnabled(False)
         self.przegladaj_przycisk.setEnabled(False)
-        self.status_label.setText('Rekonstrukcja w toku...')
         self.progress_bar.setVisible(False)
         self.progress_bar.setRange(0, 0)
 
@@ -355,7 +430,6 @@ class MainWindow(QMainWindow):
         self.extract_btn.setEnabled(True)
         self.przegladaj_przycisk.setEnabled(True)
         self.progress_bar.setVisible(False)
-        self.status_label.setText(f'Rekonstrukcja zakończona! Model zapisano jako {nazwa_modelu}')
         QMessageBox.information(self, 'Sukces', f'Rekonstrukcja zakończona! Model zapisano jako {nazwa_modelu}')
 
     def colmap_error(self, komunikat_bledu):
@@ -363,10 +437,7 @@ class MainWindow(QMainWindow):
         self.extract_btn.setEnabled(True)
         self.przegladaj_przycisk.setEnabled(True)
         self.progress_bar.setVisible(False)
-        self.status_label.setText('Błąd podczas rekonstrukcji')
         QMessageBox.critical(self, 'Błąd', f'Wystąpił błąd podczas rekonstrukcji: {komunikat_bledu}')
-
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
